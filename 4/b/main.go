b21d39b28e47164a7c55862b9e9466d29cb29b1d
greghvk <mrgreg557@gmail.com>
2023-12-08 17:56:44 +0100

    add gitignore + change indentation to spaces

--- 4/b/main.go
+++ 4/b/main.go
@@ -18,69 +18,69 @@ func main() {
   defer file.Close()
 
   scanner := bufio.NewScanner(file)
-	res := 0
+  res := 0
 
-	var counts []int
+  var counts []int
 
 
-	i := 0
+  i := 0
   for scanner.Scan() {
-		if i == len(counts) {
-			counts = append(counts, 1)
-		} else {
-			counts[i]++
-		}
+    if i == len(counts) {
+      counts = append(counts, 1)
+    } else {
+      counts[i]++
+    }
     res += counts[i]
-		// fmt.Println("card ", i, " has ", counts[i])
-		numMatches := solve(scanner.Text())
-		// fmt.Println("num matches is ", numMatches)
-		for j := 1; j<=numMatches; j++ {
-			if i+j == len(counts) {
-				counts = append(counts, 0)
-			}
-			counts[i+j] += counts[i]
-		}
-		i++
+    // fmt.Println("card ", i, " has ", counts[i])
+    numMatches := solve(scanner.Text())
+    // fmt.Println("num matches is ", numMatches)
+    for j := 1; j<=numMatches; j++ {
+      if i+j == len(counts) {
+        counts = append(counts, 0)
+      }
+      counts[i+j] += counts[i]
+    }
+    i++
   }
-	fmt.Println(res)
+  fmt.Println(res)
 }
 
 func solve(s string) int {
-	s = strings.Split(s, ":")[1]
-	split := strings.Split(s, "|")
-	m := makeMap(parseNums(split[0]))
-	return count(parseNums(split[1]), m)
+  s = strings.Split(s, ":")[1]
+  split := strings.Split(s, "|")
+  m := makeMap(parseNums(split[0]))
+  return count(parseNums(split[1]), m)
 }
 
 func parseNums(s string) []int {
-	nums := strings.Split(s, " ")
-	var res []int
-	for _, el := range(nums) {
-		i, err := strconv.Atoi(el)
-		if err != nil {
-			continue
-		}
-		res = append(res, i)
-	}
-	return res
+  nums := strings.Split(s, " ")
+  var res []int
+  for _, el := range(nums) {
+    i, err := strconv.Atoi(el)
+    if err != nil {
+      continue
+    }
+    res = append(res, i)
+  }
+  return res
 }
 
 func makeMap(nums []int) map[int]bool {
-	m := make(map[int]bool)
-	for _, el := range(nums) {
-		m[el] = true
-	}
-	return m
+  m := make(map[int]bool)
+  for _, el := range(nums) {
+    m[el] = true
+  }
+  return m
 }
 
 func count(nums []int, m map[int]bool) int {
-	res := 0
+  res := 0
 
   for _, el := range(nums) {
-		if _, ok := m[el]; ok {
-			res++
-		}
-	}
+    if _, ok := m[el]; ok {
+      res++
+    }
+  }
 
-	return res
+  return res
 }
--- 5/a/main.go
+++ 5/a/main.go
@@ -21,85 +21,85 @@ func main() {
 
   scanner := bufio.NewScanner(file)
 
-	scanner.Scan()
-	els := strings.Split(scanner.Text(), ": ")
-	fmt.Println(els)
+  scanner.Scan()
+  els := strings.Split(scanner.Text(), ": ")
+  fmt.Println(els)
   nums:= parseStrings(strings.Split(els[1], " "))
-	fmt.Println(nums)
+  fmt.Println(nums)
 
 
-	scanner.Scan()
-	for {
-		mapping := getMapping(scanner)
-		if len(mapping) == 0 {
-			break
-		}
-		nums = applyMapping(nums, mapping)
-	}
-	fmt.Println("final nums ", nums)
-	min := nums[0]
-	for _, el := range(nums) {
-		if el < min {
-			min = el
-		}
-	}
-	fmt.Println(min)
+  scanner.Scan()
+  for {
+    mapping := getMapping(scanner)
+    if len(mapping) == 0 {
+      break
+    }
+    nums = applyMapping(nums, mapping)
+  }
+  fmt.Println("final nums ", nums)
+  min := nums[0]
+  for _, el := range(nums) {
+    if el < min {
+      min = el
+    }
+  }
+  fmt.Println(min)
 }
 
 func parseStrings(s []string) []int64 {
-	var res []int64
-	for _, cur := range(s) {
-		res = append(res, mustParse(cur))
-	}
-	return res
+  var res []int64
+  for _, cur := range(s) {
+    res = append(res, mustParse(cur))
+  }
+  return res
 }
 
 func mustParse(s string) int64 {
-	res, err := strconv.ParseInt(s, 10, 64)
-	if err != nil {
-		panic(err)
-	}
-	return res
+  res, err := strconv.ParseInt(s, 10, 64)
+  if err != nil {
+    panic(err)
+  }
+  return res
 }
 
 func getMapping(scanner *bufio.Scanner) [][]int64 {
-	var res [][]int64 
-	if !scanner.Scan() {
-		return res
-	}
-	for scanner.Scan() && len(scanner.Text()) > 0 {
-		els := parseStrings(strings.Split(scanner.Text(), " "))
-		res = append(res, []int64{els[1], els[0], els[2]})
-	}
-	sort.Slice(res, func(i, j int) bool {
-		return res[i][0] < res[j][0]
-	})
-	return res
+  var res [][]int64 
+  if !scanner.Scan() {
+    return res
+  }
+  for scanner.Scan() && len(scanner.Text()) > 0 {
+    els := parseStrings(strings.Split(scanner.Text(), " "))
+    res = append(res, []int64{els[1], els[0], els[2]})
+  }
+  sort.Slice(res, func(i, j int) bool {
+    return res[i][0] < res[j][0]
+  })
+  return res
 }
 
 func applyMapping(srcs []int64, mapping [][]int64) []int64 {
-	sort.Slice(srcs, func(i, j int) bool {
-		return srcs[i] < srcs[j]
-	})
-	var res []int64
-	srcPos := 0
-	mapPos := 0
-	// fmt.Println("using mapping ", mapping)
+  sort.Slice(srcs, func(i, j int) bool {
+    return srcs[i] < srcs[j]
+  })
+  var res []int64
+  srcPos := 0
+  mapPos := 0
+  // fmt.Println("using mapping ", mapping)
 
-	for srcPos < len(srcs) {
-		for mapPos < len(mapping) && mapping[mapPos][0] + mapping[mapPos][2] < srcs[srcPos] {
-			mapPos++
-		}
+  for srcPos < len(srcs) {
+    for mapPos < len(mapping) && mapping[mapPos][0] + mapping[mapPos][2] < srcs[srcPos] {
+      mapPos++
+    }
 
-		if mapPos < len(mapping) && mapping[mapPos][0] <= srcs[srcPos] {
-			diff := srcs[srcPos] - mapping[mapPos][0]
-			res = append(res, mapping[mapPos][1] + diff)
-			// fmt.Println("mapping ", srcs[srcPos], " to ", mapping[mapPos][1] + diff)
-		} else {
-			// fmt.Println("mapping ", srcs[srcPos], " to itself")
-			res = append(res, srcs[srcPos])
-		}
-		srcPos++
-	}
-	return res
+    if mapPos < len(mapping) && mapping[mapPos][0] <= srcs[srcPos] {
+      diff := srcs[srcPos] - mapping[mapPos][0]
+      res = append(res, mapping[mapPos][1] + diff)
+      // fmt.Println("mapping ", srcs[srcPos], " to ", mapping[mapPos][1] + diff)
+    } else {
+      // fmt.Println("mapping ", srcs[srcPos], " to itself")
+      res = append(res, srcs[srcPos])
+    }
+    srcPos++
+  }
+  return res
 }
